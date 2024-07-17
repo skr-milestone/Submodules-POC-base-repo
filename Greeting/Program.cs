c5492e7a3cc2ac52682486474d08a8f6686dd657
skr-milestone <skr@milestone.dk>
2024-07-17 13:28:31 +0300

    Make all text uppercase

--- Greeting/Program.cs
+++ Greeting/Program.cs
@@ -7,9 +7,9 @@ namespace Submodule.Poc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, {Constants.Name}");
+            Console.WriteLine($"HELLO, {Constants.Name}");
 
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("Press any key to exit.".ToUpper());
             Console.ReadKey();
         }
     }
